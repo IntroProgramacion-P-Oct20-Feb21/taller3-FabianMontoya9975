3a8f9e0331ff87101499d7d23f50fbd7003efa9d
Fabián Montoya <jfmontoya1@utpl.edu.ec>
2020-10-30 17:42:53 -0500

    Corrección

--- Ejercicio08/src/ejercicio08/Ejercicio08.java
+++ Ejercicio08/src/ejercicio08/Ejercicio08.java
@@ -16,6 +16,8 @@ public class Ejercicio08 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        boolean resultado = true && false;
+        System.out.println(resultado);
     }
     
 }
